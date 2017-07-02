d42c72016c97028c696050e0f0f388289685c5a6
Gregory Griffin <mr.gregory.griffin@gmail.com>
2017-07-03 00:34:12 +0200

    Use ASCII art to create initials

---
+++ YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials
+{
+  public static void main ( String[] args )
+  {
+    System.out.println(" GGG   V   V   GGG ");
+    System.out.println("G   G  V   V  G   G");
+    System.out.println("G      V   V  G    ");
+    System.out.println("GGGGG  V   V  GGGGG");
+    System.out.println("G   G  V   V  G   G");
+    System.out.println("G   G   V V   G   G");
+    System.out.println(" GGG     V     GGG");
+  }
+}
