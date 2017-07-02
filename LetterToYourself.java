d3be4eb280c87f029ba17b96f5302cd32ae94b67
Gregory Griffin <mr.gregory.griffin@gmail.com>
2017-07-03 00:23:54 +0200

    Add more lines and text to envelope

--- LetterToYourself.java
+++ LetterToYourself.java
@@ -2,16 +2,17 @@ public class LetterToYourself
 {
   public static void main ( String[] args )
   {
-    System.out.println("+--------------------------------------+");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("|                                      |");
-    System.out.println("+--------------------------------------+");
+    System.out.println("+----------------------------------------------------------+");
+    System.out.println("|                                                     #### |");
+    System.out.println("|                                                     #### |");
+    System.out.println("|                                                     #### |");
+    System.out.println("|                                                          |");
+    System.out.println("|                                                          |");
+    System.out.println("|                            Gregory Griffin               |");
+    System.out.println("|                            Facebook HQ                   |");
+    System.out.println("|                            1 Hacker Way,                 |");
+    System.out.println("|                            Menlo Park, CA 94025          |");
+    System.out.println("|                                                          |");
+    System.out.println("+----------------------------------------------------------+");
   }
 }
