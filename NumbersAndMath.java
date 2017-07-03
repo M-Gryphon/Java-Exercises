891aadf4923ead1b6f5875dd43275ae86b07a545
Gregory Griffin <mr.gregory.griffin@gmail.com>
2017-07-03 02:29:08 +0200

    Play around numbers and math operators

---
+++ NumbersAndMath.java
@@ -0,0 +1,43 @@
+public class NumbersAndMath
+{
+  public static void main ( String[] args )
+  {
+    //Print the string "I will now count my chickens:"
+    System.out.println("I will now count my chickens:");
+
+    //Print the string Hens and calculate the value of ( 25 + 30 / 6)
+    System.out.println("Hens " + ( 25 + 30 / 6) );
+
+    //Print the string Roosters and calculate the value of ( 100 - 25 * 3 % 4 )
+    System.out.println("Roosters " + ( 100 - 25 * 3 % 4 ) );
+
+    //Print the string, "Now I will count the eggs:"
+    System.out.println("Now I will count the eggs:");
+
+    //Calculate the value of ( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+    System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+
+    //Print the string "Is it true that 3 + 2 < 5 - 7?"
+    System.out.println("Is it true that 3 + 2 < 5 - 7?");
+
+    //Calculate the value of ( 3 + 2 < 5 -7 )
+    System.out.println( 3 + 2 < 5 -7 );
+
+    //Print the string "What is 3 + 2? " and calculate the value of ( 3 + 2 )
+    System.out.println("What is 3 + 2? " + ( 3 + 2 ) );
+    //Print the string "What is 5 - 7? " and calculate the value of ( 5 - 7 )
+    System.out.println("What is 5 - 7? " + ( 5 - 7 ) );
+
+    //Print the string "Oh, that's why it's false."
+    System.out.println("Oh, that's why it's false.");
+    //Print the string "How about some more."
+    System.out.println("How about some more.");
+
+    //Print the string "Is it greater? " and calculate the value of ( 5 > -2 )
+    System.out.println("Is it greater? " + ( 5 > -2 ) );
+    //Print the string "Is it greater or equal? " and calculate the value of ( 5 >= 2 )
+    System.out.println("Is it greater or equal? " + ( 5 >= 2 ) );
+    //Print the string "Is it less or equal? " and calculate the value of ( 5 <= -2 )
+    System.out.println("Is it less or equal? " + ( 5 <= -2 ) );
+  }
+}
