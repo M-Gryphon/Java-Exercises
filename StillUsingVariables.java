7a92bf315f7c33aed95e453eef29596c3a560c78
Gregory Griffin <mr.gregory.griffin@gmail.com>
2017-07-03 22:12:29 +0200

    Store int and String data in variables and print them out

---
+++ StillUsingVariables.java
@@ -0,0 +1,15 @@
+public class StillUsingVariables
+{
+  public static void main ( String[] args )
+  {
+    // Declare variables
+    int gradYear;
+    String myName;
+
+    // Initialize variables
+    gradYear = 2016;
+    myName = "Gregory Griffin";
+
+    System.out.println("My name is " + myName + " and I graduated from Le Wagon's Full Stack Web Developement program in " + gradYear + ".");
+  }
+}
